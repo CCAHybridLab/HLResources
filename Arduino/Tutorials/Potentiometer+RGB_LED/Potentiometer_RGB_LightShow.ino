ccc998236a73340f6adb4b5e5e140d1c675f1bbb
tyah-amoy <tyah-amoy@cca.edu>
2025-10-08 10:57:26 -0700

    Update Potentiometer_RGB_LightShow.ino

--- Arduino/Tutorials/Potentiometer+RGB_LED/Potentiometer_RGB_LightShow.ino
+++ Arduino/Tutorials/Potentiometer+RGB_LED/Potentiometer_RGB_LightShow.ino
@@ -7,12 +7,8 @@
   const int gPotPin = A2;
   const int bPotPin = A4;
 
-//Variables:
-  int valueRed;
-  int valueGreen;
-  int valueBlue;
-
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(rLedPin, OUTPUT);
   pinMode(gLedPin, OUTPUT);
@@ -25,16 +21,56 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  valueRed = analogRead(rPotPin);
-  valueRed = map(valueRed, 0, 1023, 0, 255);
+  updateRed();
+  updateGreen();
+  updateBlue();
+
+  delay(200);
+}
+
+void updateRed() {
+  int valueRedPot = analogRead(rPotPin);
+  int valueRed = map(valueRedPot, 100, 950, 0, 255);
+
+  // Serial.print("RedPot: ");
+  // Serial.println(valueRedPot);
+  // Serial.print("RedLEDVal: ");
+  // Serial.println(valueRed);
+  // Serial.println("----------");
+
+  if (valueRedPot <= 100) {
+    valueRed = 0;
+  }
+
   analogWrite(rLedPin, valueRed);
-  delay(50);
-  valueGreen = analogRead(gPotPin);
-  valueGreen = map(valueGreen, 0, 1023, 0, 255);
+  delay(100);
+}
+
+void updateGreen() {
+  int valueGreenPot = analogRead(gPotPin);
+  int valueGreen = map(valueGreenPot, 100, 950, 0, 255);
+
+  if (valueGreenPot <= 100) {
+    valueGreen = 0;
+  }
+
   analogWrite(gLedPin, valueGreen);
-  delay(50);
-  valueBlue = analogRead(bPotPin);
-  valueBlue = map(valueBlue, 0, 1023, 0, 255);
+  delay(100);
+}
+
+void updateBlue() {
+  int valueBluePot = analogRead(bPotPin);
+  int valueBlue = map(valueBluePot, 100, 950, 0, 255);
+
+  Serial.print("BluePot: ");
+  Serial.println(valueBluePot);
+  Serial.print("BlueLEDVal: ");
+  Serial.println(valueBlue);
+  Serial.println("----------");
+
+  if (valueBluePot <= 100) {
+    valueBlue = 0;
+  }
   analogWrite(bLedPin, valueBlue);
-  delay(50);
+  delay(100);
 }
